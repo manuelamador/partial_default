b9fe98926be06837bf445a84b34334b8edd614c7
Manuel Amador <amador.manuel@gmail.com>
2022-04-01 13:27:20 -0500

    updated figure generation

--- scripts/paper_figures.jl
+++ scripts/paper_figures.jl
@@ -27,6 +27,196 @@ sol.cstar
 
 # ## Plots
 
+# +
+using Plots
+
+# Using PGF plots 
+pgfplotsx(); 
+default(labels = "", lw = 3, color = :black) 
+
+
+# +
+function do_plots(s; tmax = 100)
+    tspan = collect(range(0, tmax, length=200))
+    q_list = [s.q(t) for t in tspan]
+
+    f1 = plot()
+    f2 = plot()
+    f3 = plot()
+    f4 = plot()
+
+    plot!(f1, [(t, q) for (t, q) in zip(tspan, q_list) if 0 < q < 1], 
+    title = L"Bond price, $q(t)$", xlim = (0, tmax))
+    plot!(f2, tspan, [s.b(t) for t in tspan], title = L"Debt level, $b(t)$")
+    plot!(f3, tspan, [s.ρ(t) for t in tspan], title = L"Reputation, $\rho(t)$")
+    plot!(f4, [(t, get_c(s, t)) for t in tspan if 0 < get_c(s, t) < 1.2], 
+            title = L"Consumption, $c(t)$", xlim = (0, tmax))
+        
+    for f in (f1, f2, f3, f4)
+        vline!(f, [s.T], color = :gray, lw = 2.0)
+        # xlabel!(f, "time")
+        xticks!(f, [0, 20, 40, 60, 80, 100, s.T], ["0", "20", "40", "60", "80", "100", "T"])
+    end
+
+    plot(f1, f2, f3, f4, layout = (2, 2), size = 0.8 .* [900, 600])
+end 
+
+
+function do_conditional_default_probability_plots(s; maxT = 100, npoints = 200)
+    tmax = min(maxT, s.tmax)
+
+    f2 = plot()
+    
+    f3 = plot()
+    
+    for f in (f2, f3)
+        vline!(f, [s.T], color = :gray, lw = 2.0)
+        # xlabel!(f, "time")
+        xticks!(f, [0, 20, 40, 60, 80, 100, s.T], ["0", "20", "40", "60", "80", "100", "T"])
+    end
+
+    γss = [(t, get_γs(s, t)) for t in range(s.T, tmax, length = npoints)]
+
+    plot!(f2, title = L"Default probabilities, $\gamma_n(t)$", xlim = (0, tmax), ylim = (0, 0.7))
+
+    plot!(f2, [(t, get_γ0(s, t)) for t in range(s.T, tmax, length = npoints)],
+        label = L"$\gamma_0$ (full default)",
+        color = :black)
+
+    for (i, sty) in zip(1:length(γss[1][2]), (:dash, :dot))
+        η = s.model.η_grid[i]
+        plot!(f2, [(t, x[i]) for (t, x) in γss], 
+            label = L"$\gamma_%$i (\eta = %$η$: haircut $%$(Int(100*(1-η)))%)$",
+            style = sty, 
+            color = :black,
+            lw = 1,
+            legend_style = "{at = {(0.5,0.3)}, anchor = west}", 
+            extra_kwargs = :subplot
+        )
+    end
+    
+    αss = [(t, get_αs(s, t)) for t in range(0, s.T - 0.001, length = npoints)]
+
+
+    plot!(f3, title = L"Arrival rates of defaults, $\alpha_n(t)$", xlim = (0, tmax), ylim = (0, 0.5))
+    
+    ymax = 0.5
+    plot!(f3, [(t, get_α0(s, t)) for t in range(0, s.T - 0.001, length = npoints) 
+                if get_α0(s, t) < ymax], 
+        label = L"$\alpha_0$ (full default)", 
+        color = :black
+    )
+    
+    for (i, sty) in zip(1:length(γss[1][2]), (:dash, :dot))
+        η = (s.model.η_grid[i])
+        plot!(f3, [(t, x[i]) for (t, x) in αss if x[1] < ymax], 
+            label=L"$\alpha_%$i (\eta = %$η$: haircut $%$(Int(100*(1-η)))%)$",
+            style = sty,
+            color = :black,
+            lw = 1,
+            legend_style = "{at = {(0.5,0.3)}, anchor = west}", 
+            extra_kwargs = :subplot
+        )
+    end
+    
+    plot(f3, f2, layout = (1, 2), size = (900, 300))
+end 
+
+
+
+function do_unconditional_default_probability_plots(s; maxT = 100, npoints = 200)
+    tmax = min(maxT, s.tmax)
+    
+    (; δ, η_grid, θn) = s.model
+    (; ρ) = s
+
+    unconditional_hazard = [(t, θn .+ δ .* get_γs(s, t)) 
+                                for t in range(s.T, tmax, length = npoints)]
+   
+    
+    f2 = plot(title = L"$\theta_n + \delta \gamma_n(t)$", xlim = (0, tmax), legend=:topleft)
+
+    plot!(f2, [(t, δ * get_γ0(s, t)) for t in range(s.T, tmax, length = npoints)],
+        label = L"$\delta \gamma_0$",
+        color = :black)
+
+    for (i, sty) in zip(1:length(unconditional_hazard[1][2]), (:dash, :dot))
+        η = η_grid[i]
+        plot!(f2, [(t, x[i]) for (t, x) in unconditional_hazard], 
+            label = L"$(\eta = %$η)$",
+            style = sty, 
+            color = :black,
+            lw = 1
+        )
+    end
+    
+    unconditional_proba = [(t, θn .+ (1 - ρ(t)) .* get_αs(s, t)) for t in range(0, s.T - 0.001, length = npoints)]
+    
+    f3 = plot(
+        title = L"$\rho(t) \theta_n + (1 - \rho(t))( \theta_n + \alpha_n(t))$", xlim = (0, tmax), 
+        legend = :topright)
+    ymax = 1.0
+
+    plot!(f3, [(t, (1 - ρ(t)) * get_α0(s, t)) for t in range(0, s.T - 0.0001, length = npoints) 
+                if get_α0(s, t) < ymax], 
+        label = L"$(1 - \rho(t)) \alpha_0$", 
+        color = :black)
+
+    for (i, sty) in zip(1:length(unconditional_proba[1][2]), (:dash, :dot))
+        η = η_grid[i]
+        plot!(f3, [(t, x[i]) for (t, x) in unconditional_proba if x[1] < ymax], 
+            label = L"$(\eta = %$η)$",
+            style = sty,
+            color = :black,
+            lw = 1
+        )
+    end
+    
+    for f in (f2, f3)
+        vline!(f, [s.T], color = :gray, lw = 2.0)
+        # xlabel!(f, "time")
+        xticks!(f, [0, 20, 40, 60, 80, 100, s.T], ["0", "20", "40", "60", "80", "100", "T"])
+    end
+    
+    plot(f3, f2, layout = (1, 2), size = (900, 300))
+end 
+
+
+function yield(s, q)
+    (; i, λ) = s.model 
+    return (i + λ) / q - λ
+end 
+
+
+function do_increase_in_yield_plots(s; maxT = 100, npoints = 200)
+
+    (; η_grid, bmin) = s.model 
+
+    spread = function(t, η)
+        before_yield = yield(s, s.q(t))
+        after_t = find_zero(τ -> s.b(τ) - η * s.b(t), (0, t))
+        after_yield = yield(s, s.q(after_t))
+        return (after_yield - before_yield)
+    end 
+
+    tmax = min(maxT, s.tmax)
+
+    f1 = plot(size = (450, 300), 
+        title = "Change in yield after haircut",
+        ylabel = L"\%")
+    for (η, c) ∈ zip(η_grid, (:black, :gray))
+        ser = [(t, 100 * spread(t, η))  for t in range(bmin, tmax, length = npoints)]
+        plot!(f1, ser, color = c)
+        annotate!(f1, ser[end][1] - 18, ser[end][2] + 0.5, L"\small $\eta = %$η$ $($haircut $%$(Int(100*(1-η)))%)$")
+    end 
+    vline!(f1, [s.T], color = :gray, lw = 2.0)
+
+    return f1
+
+end 
+
+# -
+
 f1 = do_plots(sol)
 
 f2 = do_conditional_default_probability_plots(sol)
--- src/partial_default.jl
+++ src/partial_default.jl
@@ -7,11 +7,6 @@ using Memoization
 using DifferentialEquations
 import DifferentialEquations: solve
 using LaTeXStrings
-using Plots
-
-# Using PGF plots 
-pgfplotsx(); 
-default(labels = "", lw = 3, color = :black) 
 
 # The maximum range of t for the solutions of the O 
 const _tmax = 300.0
@@ -106,7 +101,7 @@ end
 # If b < bmin, it is assumed that no haircuts occur and thus the summation term is 
 # zero. 
 function get_summation_term(model; c, ρ, b, q, h, p, t, bT, T, inv_b)
-    @unpack η_grid, θn, bmin = model
+    (; η_grid, θn, bmin) = model
     acc = zero(c)
     if b > bmin  
         for (n, θ) in enumerate(θn)
@@ -123,7 +118,7 @@ end
 # The differential equation before t
 # In this case, b' and q' are immediately obtained from the constant consumption guess
 function DDE_before_T(model, c, inv_b)
-    @unpack ϵ, i, λ, δ = model
+    (; ϵ, i, λ, δ) = model
     f = @closure (du, u, h, p, t) -> begin 
         (ρ, b, _) = u  # current state
         q_prime = qPrime(model, b, c) 
@@ -289,9 +284,9 @@ function _get_γ0(t, model, sol)
 end 
 
 function _get_α0(t, model, sol)
-    @unpack b, q, ρ, ρb, ode_sols, cstar = sol 
-    @unpack τb_before_T = ode_sols
-    @unpack i, λ = model
+    (; b, q, ρ, ρb, ode_sols, cstar) = sol 
+    (; τb_before_T) = ode_sols
+    (; i, λ) = model
     sum_term = zero(cstar)
     for (η, θ) in zip(model.η_grid, model.θn) 
         sum_term += (q(t) - q(τb_before_T(b(t) * η)) * η) * ρ(t) * θ / ρb(η * b(t))
@@ -313,173 +308,3 @@ function solve(model::FixedHaircutsModel; tmax = _tmax, c_range = [1.002, 1.05])
     return (sol..., cstar = cstar, tmax = tmax)
 end
 
-## Plots 
-
-
-function do_plots(s; tmax = 100)
-    tspan = collect(range(0, tmax, length=200))
-    q_list = [s.q(t) for t in tspan]
-
-    f1 = plot()
-    f2 = plot()
-    f3 = plot()
-    f4 = plot()
-
-    plot!(f1, [(t, q) for (t, q) in zip(tspan, q_list) if 0 < q < 1], 
-    title = L"$q(t)$", xlim = (0, tmax))
-    plot!(f2, tspan, [s.b(t) for t in tspan], title = L"$b(t)$")
-    plot!(f3, tspan, [s.ρ(t) for t in tspan], title = L"$\rho(t)$")
-    plot!(f4, [(t, get_c(s, t)) for t in tspan if 0 < get_c(s, t) < 1.2], 
-            title = L"$c(t)$", xlim = (0, tmax))
-        
-    for f in (f1, f2, f3, f4)
-        vline!(f, [s.T], color = :gray, lw = 2.0)
-        # xlabel!(f, "time")
-        xticks!(f, [0, 20, 40, 60, 80, 100, s.T], ["0", "20", "40", "60", "80", "100", "T"])
-    end
-
-    plot(f1, f2, f3, f4, layout = (2, 2), size = 0.8 .* [900, 600])
-end 
-
-
-function do_conditional_default_probability_plots(s; maxT = 100, npoints = 200)
-    tmax = min(maxT, s.tmax)
-
-    f2 = plot()
-    f3 = plot()
-
-    γss = [(t, get_γs(s, t)) for t in range(s.T, tmax, length = npoints)]
-
-    plot!(f2, title = L"$\gamma_n(t)$", xlim = (0, tmax), legend=:topleft)
-
-    plot!(f2, [(t, get_γ0(s, t)) for t in range(s.T, tmax, length = npoints)],
-        label = L"$\gamma_0$",
-        color = :black)
-
-    for (i, sty) in zip(1:length(γss[1][2]), (:dash, :dot))
-        η = s.model.η_grid[i]
-        plot!(f2, [(t, x[i]) for (t, x) in γss], 
-            label = L"$\gamma_%$i (\eta = %$η)$",
-            style = sty, 
-            color = :black,
-            lw = 1
-        )
-    end
-    
-    αss = [(t, get_αs(s, t)) for t in range(0, s.T - 0.001, length = npoints)]
-    
-    plot!(f3, title = L"$\alpha_n(t)$", xlim = (0, tmax), legend = :topright)
-    ymax = 0.5
-    plot!(f3, [(t, get_α0(s, t)) for t in range(0, s.T - 0.001, length = npoints) 
-                if get_α0(s, t) < ymax], 
-        label = L"$\alpha_0$", 
-        color = :black)
-    for (i, sty) in zip(1:length(γss[1][2]), (:dash, :dot))
-        η = (s.model.η_grid[i])
-        plot!(f3, [(t, x[i]) for (t, x) in αss if x[1] < ymax], 
-            label=L"$\alpha_%$i (\eta = %$η)$",
-            style = sty,
-            color = :black,
-            lw = 1
-        )
-    end
-    
-    for f in (f2, f3)
-        vline!(f, [s.T], color = :gray, lw = 2.0)
-        # xlabel!(f, "time")
-        xticks!(f, [0, 20, 40, 60, 80, 100, s.T], ["0", "20", "40", "60", "80", "100", "T"])
-    end
-    
-    plot(f3, f2, layout = (1, 2), size = (900, 300))
-end 
-
-
-
-function do_unconditional_default_probability_plots(s; maxT = 100, npoints = 200)
-    tmax = min(maxT, s.tmax)
-    
-    @unpack δ, η_grid, θn = s.model
-    @unpack ρ = s
-
-    unconditional_hazard = [(t, θn .+ δ .* get_γs(s, t)) 
-                                for t in range(s.T, tmax, length = npoints)]
-   
-    
-    f2 = plot(title = L"$\theta_n + \delta \gamma_n(t)$", xlim = (0, tmax), legend=:topleft)
-
-    plot!(f2, [(t, δ * get_γ0(s, t)) for t in range(s.T, tmax, length = npoints)],
-        label = L"$\delta \gamma_0$",
-        color = :black)
-
-    for (i, sty) in zip(1:length(unconditional_hazard[1][2]), (:dash, :dot))
-        η = η_grid[i]
-        plot!(f2, [(t, x[i]) for (t, x) in unconditional_hazard], 
-            label = L"$(\eta = %$η)$",
-            style = sty, 
-            color = :black,
-            lw = 1
-        )
-    end
-    
-    unconditional_proba = [(t, θn .+ (1 - ρ(t)) .* get_αs(s, t)) for t in range(0, s.T - 0.001, length = npoints)]
-    
-    f3 = plot(title = L"$\rho(t) \theta_n + (1 - \rho(t))( \theta_n + \alpha_n(t))$", xlim = (0, tmax), legend = :topright)
-    ymax = 1.0
-
-    plot!(f3, [(t, (1 - ρ(t)) * get_α0(s, t)) for t in range(0, s.T - 0.0001, length = npoints) 
-                if get_α0(s, t) < ymax], 
-        label = L"$(1 - \rho(t)) \alpha_0$", 
-        color = :black)
-
-    for (i, sty) in zip(1:length(unconditional_proba[1][2]), (:dash, :dot))
-        η = η_grid[i]
-        plot!(f3, [(t, x[i]) for (t, x) in unconditional_proba if x[1] < ymax], 
-            label = L"$(\eta = %$η)$",
-            style = sty,
-            color = :black,
-            lw = 1
-        )
-    end
-    
-    for f in (f2, f3)
-        vline!(f, [s.T], color = :gray, lw = 2.0)
-        # xlabel!(f, "time")
-        xticks!(f, [0, 20, 40, 60, 80, 100, s.T], ["0", "20", "40", "60", "80", "100", "T"])
-    end
-    
-    plot(f3, f2, layout = (1, 2), size = (900, 300))
-end 
-
-
-function yield(s, q)
-    @unpack i, λ = s.model 
-    return (i + λ) / q - λ
-end 
-
-
-function do_increase_in_yield_plots(s; maxT = 100, npoints = 200)
-
-    @unpack η_grid, bmin = s.model 
-
-    spread = function(t, η)
-        before_yield = yield(s, s.q(t))
-        after_t = find_zero(τ -> s.b(τ) - η * s.b(t), (0, t))
-        after_yield = yield(s, s.q(after_t))
-        return (after_yield - before_yield)
-    end 
-
-    tmax = min(maxT, s.tmax)
-
-    f1 = plot(size = (450, 300), 
-        title = "Change in yield after haircut",
-        ylabel = L"\%")
-    for (η, c) ∈ zip(η_grid, (:black, :gray))
-        ser = [(t, 100 * spread(t, η))  for t in range(bmin, tmax, length = npoints)]
-        plot!(f1, ser, color = c)
-        annotate!(f1, ser[end][1] - 10, ser[end][2] + 0.5, L"$\eta = %$η$")
-    end 
-    vline!(f1, [s.T], color = :gray, lw = 2.0)
-
-    return f1
-
-end 
